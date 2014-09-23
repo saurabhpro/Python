e3fd6760714bd453c9df364c2b3fffaa8f18e98d
Saurabh Kumar <Saurabhk15@outlook.com>
2014-09-23 22:59:41 +0530

    1.2 escape sequence in python
    python is very forgiving

--- Hello/Hello/Hello.py
+++ Hello/Hello/Hello.py
@@ -1,2 +1,14 @@
 print("Here's your File" + 'Hello "ur" World' + """\nthis is NOT ignored now even if we use triple quotes""");
 print ("""here "a" is very 'a' """)
+
+#what to do to print \
+print (" hello world \ saurabh ")
+
+#what if \news etc 
+print ("hello www:\\hello.com\new")
+#prints
+#hello www:\hello.com
+#ews
+print ("hello www:\\\hello.com\\new")
+#prints
+#hello www:\\hello.com\news
